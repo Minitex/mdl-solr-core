e1e24dd50afa91c895434f5d780dfbdda3668568
Dan Olson <olson_dan@yahoo.com>
2023-12-26 13:16:36 -0500

    Update README and rebuild script with current image repository

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker image rmi -f mdl_solr:dev; docker build . --tag mdl_solr:dev --no-cache;
-docker image rmi -f mdl_solr:test; docker build . --tag mdl_solr:test --no-cache
+docker image rmi -f minitex/mdl_solr:latest; docker build . --tag minitex/mdl_solr:latest --no-cache
